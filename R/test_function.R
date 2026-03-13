db3454ad270e6bae4e0fa49dd565bd8bc67bb45d
Lin Guo <lguo@corevitas.com>
2026-03-13 10:21:48 -0500

    update test function

--- R/test_function.R
+++ R/test_function.R
@@ -3,11 +3,11 @@
 #' @param num1 first number
 #' @param num2 second number
 #'
-#' @returns A sum of the 2 numbers
+#' @returns A sum of num1 + num2 squared
 #' @export
 #'
 #' @examples test_function(num1=5, num2=6)
 test_function <- function(num1, num2){
-  result<- num1 + num2
+  result<- num1 + num2 **2
   return(result)
 }
