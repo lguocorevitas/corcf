40dd3c500ea776becc2ce37a021b41a203de314f
Lin Guo <lguo@corevitas.com>
2026-03-13 10:07:40 -0500

    testing

---
+++ R/test_function.R
@@ -0,0 +1,13 @@
+#' Test function for educational purposes
+#'
+#' @param num1 first number
+#' @param num2 second number
+#'
+#' @returns A sum of the 2 numbers
+#' @export
+#'
+#' @examples test_function(num1=5, num2=6)
+test_function <- function(num1, num2){
+  result<- num1 + num2
+  return(result)
+}
