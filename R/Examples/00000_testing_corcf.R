b5d55453a6939ebf252f179c461f7c11c244afe5
Lin Guo <lguo@corevitas.com>
2026-05-01 08:48:38 -0500

    adjust examples

--- R/Examples/00000_testing_corcf.R
+++ R/Examples/00000_testing_corcf.R
@@ -14,7 +14,7 @@ library(roxygen2)
 # roxygen2::roxygenise()
 # install.packages("devtools")
 # devtools::document()
-detach("package:corcf", unload = TRUE, character.only = TRUE)
+
 remotes::install_github("lguocorevitas/corcf", force = TRUE)
 library(corcf)
 ls("package:corcf")
@@ -27,51 +27,15 @@ tdy_month <- sprintf("%02d", month(tdy_date))
 cut_date    <- as.Date(glue("{tdy_year}-{tdy_month}-01"))
 sharepoint <- "~/../../Thermo Fisher Scientific/"
 dir_ra_monthly  <- glue("{sharepoint}/Biostat Data Files - RA/monthly/")
-analytic_data  <- glue("{dir_ra_monthly}/{tdy_year}/{cut_date}/")
+# analytic_data  <- glue("{dir_ra_monthly}/{tdy_year}/{cut_date}/")
 
-# data name and ids
-data_name <-"visits_calc"
-test_data <- glue("{analytic_data}/rewrite/RA_{data_name}_{cut_date}.dta")
-current_data  <- glue("{analytic_data}/2_3_keyvisitvars_{cut_date}.dta")
-id_cols <- c("id", "visitdate")
 
-master_df <- read_dta(test_data)
-using_df  <- read_dta(current_data)
-if (!"id" %in% names(using_df)) using_df <- using_df %>% mutate(id = subject_number)
-
-# run corcf function
-res <- corcf::corcf(
-  master = master_df,
-  using  = using_df,
-  vars   = "_all",
-  id     = id_cols
-  # ,verbose1 = 200
-)
-
-# optional
-# res$ecode
-# # 9 = at least one variable had differing values
-# # 106 means you hit at least one type mismatch
-# names(res$label_conflicts)
-# res$per_variable
-
-# generate a word document for report
-
-corcf::write_corcf_word(
-  res,
-  master_path = test_data,
-  using_path  = current_data,
-  id_cols     = id_cols,
-  path = glue("{analytic_data}/rewrite/QC/corcf_results_{data_name}_{tdy_date}.docx"),
-  report_date = Sys.Date()
-)
-
-# Another example, testing UAT/prod raw data: bv_subjects
+# Example 1, testing UAT/prod raw data: bv_subjects
 
 data_name <-"bv_subjects"
 
 prev_data <- glue("{dir_ra_monthly}/2026/2026-03-01/bv_raw/{data_name}.dta")
-current_data  <- glue("{analytic_data}/bv_raw/{data_name}.dta")
+current_data  <- glue("{dir_ra_monthly}/2026/2026-04-01//bv_raw/{data_name}.dta")
 id_cols <- c("subject_number")
 
 master_df <- read_dta(current_data)
@@ -116,6 +80,45 @@ corcf::write_corcf_word(
   master_path = current_data,
   using_path  = prev_data,
   id_cols     = id_cols,
-  path = glue("{analytic_data}/rewrite/QC/corcf_results_{data_name}_{tdy_date}_verbose.docx"),
+  path = glue("{dir_ra_monthly}/2026/2026-04-01/rewrite/QC/corcf_results_{data_name}_{tdy_date}_verbose.docx"),
   report_date = Sys.Date()
 )
+
+# Example 2, Testing RA Analytic data
+data_name <-"visits_calc"
+test_data <- glue("{dir_ra_monthly}/2026/2026-04-01/rewrite/RA_{data_name}_{cut_date}.dta")
+current_data  <- glue("{dir_ra_monthly}/2026/2026-04-01/2_3_keyvisitvars_{cut_date}.dta")
+id_cols <- c("id", "visitdate")
+
+master_df <- read_dta(test_data)
+using_df  <- read_dta(current_data)
+if (!"id" %in% names(using_df)) using_df <- using_df %>% mutate(id = subject_number)
+
+# run corcf function
+res <- corcf::corcf(
+  master = master_df,
+  using  = using_df,
+  vars   = "_all",
+  id     = id_cols
+  # ,verbose1 = 200
+)
+
+# optional
+# res$ecode
+# # 9 = at least one variable had differing values
+# # 106 means you hit at least one type mismatch
+# names(res$label_conflicts)
+# res$per_variable
+
+# generate a word document for report
+
+corcf::write_corcf_word(
+  res,
+  master_path = test_data,
+  using_path  = current_data,
+  id_cols     = id_cols,
+  path = glue("{dir_ra_monthly}/2026/2026-04-01/rewrite/QC/corcf_results_{data_name}_{tdy_date}.docx"),
+  report_date = Sys.Date()
+)
+
+
